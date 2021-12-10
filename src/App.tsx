5e1606abd625dfaebaec64e2099bc6b059b3576d
Kiandra Ginevra <kiandrajayne@gmail.com>
2021-12-11 04:23:33 +1100

    Initial commit of React website.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>Learn React</header>
+    </div>
+  )
+}
+
+export default App
