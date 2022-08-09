4561db885a1909d69b148bce916a57b843ede1a5
Kiandra Ginevra <kiandrajayne@gmail.com>
2022-08-09 12:44:55 +1000

    Removed unnecessary code from previous version.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import Heading from './components/Heading'
-import Projects from './components/Projects'
-import About from './components/About'
-import Skills from './components/Skills'
-import Footer from './components/Footer'
-
 const App = () => {
   return (
     <>
-      <Heading />
-      <Projects />
-      <About />
-      <Skills />
-      <Footer />
+      
     </>
   )
 }
